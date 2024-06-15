9af84acc6f5fbad95aaa52f715adb00f03cc7d40
shemil <shemilkaweesha@gmail.com>
2024-06-15 20:44:18 +0530

    Link Model updated

--- models/models.go
+++ models/models.go
@@ -8,11 +8,12 @@ type User struct {
 	Username string `json:"username"`
 	Email    string `gorm:"not null" json:"email" gorm:"unique"`
 	SubID    string `json:"sub_id" gorm:"unique"`
-	Links    []Link
+	Links    []Link `json:"links"`
 }
 
 type Link struct {
 	gorm.Model
-	Title string `json:"title" gorm:"not null"`
-	Link  string `json:"link" gorm:"not null"`
+	Title  string `json:"title" gorm:"not null"`
+	Link   string `json:"link" gorm:"not null"`
+	UserID uint   `json:"user_id"`
 }
